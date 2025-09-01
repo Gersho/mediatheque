58180ac53f9f229cba7b52150787f0c83402530f
Karim Pierre ZENNOUNE <karim-pierre.zennoune@laplateforme.io>
2025-09-01 16:54:00 +0200

    css of book show page

--- controllers/book_controller.php
+++ controllers/book_controller.php
@@ -13,16 +13,35 @@ function book_show()
         redirect('errors/404');
     }
 
+
+    // var_dump($book_info);
+
     $data = [
-        'title' => $book_info["title"],
         'author' => $book_info["author"],
         'isbn' => $book_info["isbn"],
-        'genre' => $book_info["genre"],
         'pages' => $book_info["pages"],
         'published_year' => $book_info["published_year"],
         'summary' => $book_info["summary"],
-        'stock' => $book_info["stock"]
+        'title' => $book_info["title"],
+        'genre' => $book_info["genre"],
+        'cover_path' => $book_info['cover_path'],
+        'stock' => $book_info["stock"],
+        'stylesheets' => ['assets/css/media.css']
     ];
 
     load_view_with_layout('book/show', $data);
 }
+
+
+// array(11) { 
+//     ["id"]=> int(2) 
+//     ["author"]=> string(19) "F. Scott Fitzgerald" 
+//     ["isbn"]=> string(13) "9780743273565" 
+//     ["pages"]=> int(180) 
+//     ["published_year"]=> int(1925) 
+//     ["summary"]=> string(68) "A mysterious millionaire pursues the woman he loves in the Jazz Age." 
+//     ["title"]=> string(16) "The Great Gatsby" 
+//     ["genre"]=> string(5) "Drama" 
+//     ["type"]=> string(4) "Book" 
+//     ["cover_path"]=> string(60) "https://m.media-amazon.com/images/I/91yg5rniqwL._SL1500_.jpg" 
+//     ["stock"]=> int(4) } 
--- views/book/show.php
+++ views/book/show.php
@@ -1,19 +1,41 @@
-<section>
-    <h1><?php e($title); ?></h1>
-    <p>by <?php e($author); ?> in <?php e($published_year); ?></p>
-    <p>isbn: <?php e($isbn); ?></p>
-    <p>genre: <?php e($genre); ?></p>
-    <p>pages: <?php e($pages); ?></p>
-    <p>Summary:</p>
-    <p><?php e($summary); ?> </p>
+<div class="container">
+
+    <section>
+        <div><img src="<?php e($cover_path); ?>"></div>
+
+
+        <div class="textfield">
+            <div class="titlefield">
+
+                <h1><?php e($title); ?></h1>
+            </div>
+            <div class="blabla">
+                <p>par <?php e($author); ?> en <?php e($published_year); ?></p>
+            </div>
+
+
+            <p>isbn: <?php e($isbn); ?></p>
+            <p>genre: <?php e($genre); ?></p>
+            <p>pages: <?php e($pages); ?></p>
+            <p>Resumé:</p>
+            <p><?php e($summary); ?> </p>
+
+
+        </div>
+    </section>
+    <span class="zen">
+
+        <?php
+        if ($stock !== 0):
+            ?> <button>rent</button> <?php
+        else: ?>
+            <button>sorry, this book is currently unavailable</button>
+
+        <?php endif; ?>
+    </span>
 
 
 
-    <?php
-    if ($stock !== 0):
-        ?> <button>rent</button> <?php
-    else: ?>
-        <button>sorry, this book is currently unavailable</button>
 
-    <?php endif; ?>
-</section>
+
+</div>
