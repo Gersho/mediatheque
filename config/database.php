65a40f93e5c6af65067699add4cd7ae9ba8703b7
Johan <johan.chevet@laplateforme.io>
2025-09-03 21:03:20 +0200

    add call api to get video games

--- config/database.php
+++ config/database.php
@@ -1,14 +1,13 @@
 <?php
 // Configuration de la base de données
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_NAME', 'php_mvc_app');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_CHARSET', 'utf8');
 
 // Configuration générale de l'application
-
-define('BASE_URL', 'http://localhost/mediatheque-lyn-grp05/public/');
+define('BASE_URL', 'http://localhost:8080/public');
 define('APP_NAME', 'PHP MVC Starter');
 define('APP_VERSION', '1.0.0');
 
--- controllers/debug_controller.php
+++ controllers/debug_controller.php
@@ -4,31 +4,67 @@ function debug_index()
 {
 
     if (is_post()) {
-        var_dump($_POST);
         if (isset($_POST["movies"])) {
             $response = file_get_contents("https://api.imdbapi.dev/titles?types=MOVIE&startYear=1900&sortBy=SORT_BY_USER_RATING_COUNT&sortOrder=DESC");
             if ($response) {
                 $response = json_decode($response, true);
                 $movies = $response["titles"];
                 foreach ($movies as $movie) {
-                    $data["type"] = "Movie";
-                    $data["title"] = $movie["primaryTitle"];
-                    $data["published_year"] = $movie["startYear"];
-                    $data["duration"] = $movie["runtimeSeconds"] / 60;
-                    $data["genre"] = $movie["genres"][0];
-                    $data["certification"] = "-12";
-                    $data["director"] = "dunno";
-                    $data["synopsis"] = $movie["plot"];
-                    $data["stock"] = 1;
+                    $response = file_get_contents("https://api.imdbapi.dev/titles/" . $movie["id"]);
+                    if ($response) {
+                        $movie = json_decode($response, true);
+                        $data["type"] = "Movie";
+                        $data["title"] = $movie["primaryTitle"];
+                        $data["published_year"] = $movie["startYear"];
+                        $data["duration"] = $movie["runtimeSeconds"] / 60;
+                        $data["genre"] = $movie["genres"][0];
+
+                        $array = ['Tous publics', '-12', '-16', '-18'];
+                        $rand = $array[array_rand($array)];
+                        $data["certification"] = $rand;
+                        $data["director"] = $movie["directors"][0]['displayName'];
+                        $data["synopsis"] = $movie["plot"];
+                        $data["stock"] = rand(1, 20);
+                        try {
+                            $data["cover_img"] = upload_cover_from_url($movie["primaryImage"]["url"]);
+                            insert_new_media($data, 'insert_new_movie');
+                        } catch (Exception $e) {
+                            error_logging(ErrorType::Error, $e->getMessage());
+                        }
+                    }
+                }
+            }
+        } else if (isset($_POST['games'])) {
+            $response = file_get_contents('https://api.imdbapi.dev/titles?types=VIDEO_GAME&startYear=1900&sortBy=SORT_BY_USER_RATING_COUNT&sortOrder=DESC');
+            if ($response) {
+                $response = json_decode($response, true);
+                $games = $response["titles"];
+                foreach ($games as $game) {
+                    $data["type"] = "Game";
+                    $data["title"] = $game["primaryTitle"];
+                    $data["genre"] = $game["genres"][0];
+                    $array = ['PC', 'PlayStation', 'Xbox', 'Nintendo', 'Mobile'];
+                    $rand = $array[array_rand($array)];
+                    $data["plateform"] = $rand;
+
+                    $array = ['3', '7', '12', '16', '18'];
+                    $rand = $array[array_rand($array)];
+                    $data["pegi"] = $rand;
+
+                    $data["description"] = $game["plot"];
+                    $data["stock"] = rand(1, 20);
+
+                    $array = ['Rockstar Games', 'Bungie', 'From Software', 'Bandai Namco', 'Bethesda'];
+                    $rand = $array[array_rand($array)];
+                    $data["editor"] = $rand;
                     try {
-                        $data["cover_img"] = upload_cover_from_url($movie["primaryImage"]["url"]);
-                        insert_new_media($data, 'insert_new_movie');
+                        $data["cover_img"] = upload_cover_from_url($game["primaryImage"]["url"]);
+                        insert_new_media($data, 'insert_new_game');
                     } catch (Exception $e) {
                         error_logging(ErrorType::Error, $e->getMessage());
                     }
                 }
             }
-        } else if (isset($_POST['games'])) {
         } else if (isset($_POST['books'])) {
         } elseif (isset($_POST['clean'])) {
             $sql = 'DELETE FROM medias';
