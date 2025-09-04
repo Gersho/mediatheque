624c4e0e0ca8a579b8e2331f206060acc2ce5fc6
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-09-04 09:11:51 +0200

    minor changes

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,5 +1,6 @@
 <div>
-    <a href="medias" class="btn btn-primary">Médias</a>
+    <a href="admin/medias" class="btn btn-primary">Médias</a>
     <a href="admin/users" class="btn btn-primary">Utilisateurs</a>
     <a href="admin/borrows" class="btn btn-primary">Emprunts en cours</a>
+    <a href="admin/history" class="btn btn-primary">Historique des emprunts</a>
 </div>
