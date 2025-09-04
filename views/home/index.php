3afcddbda6298f730fe881205471a5d3d7ba3db0
johan <johan.chevet@laplateforme.io>
2025-09-04 11:28:25 +0200

    clean duplicate css code

--- views/home/index.php
+++ views/home/index.php
@@ -37,17 +37,4 @@
         </div>
     <?php endforeach; ?>
 </div>
-<div class="pagination">
-    <?php if ($current_page > 1): ?>
-        <?php $link = get_page_url($current_page - 1) ?>
-        <a class="pagination-btn" href=" <?= $link ?>">❮</a>
-    <?php else: ?>
-        <div class="pagination-btn hide">❮</div>
-    <?php endif; ?>
-    <?php if ($current_page < $pages): ?>
-        <?php $link = get_page_url($current_page + 1) ?>
-        <a class="pagination-btn" href=" <?= $link ?>">❯</a>
-    <?php else: ?>
-        <div class="pagination-btn hide">❯</div>
-    <?php endif; ?>
-</div>
+<?php include_once VIEW_PATH . '/medias/pagination.php' ?>
