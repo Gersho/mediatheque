44ea9de71e8806dcf30431f32bae77e28c995e99
johan <johan.chevet@laplateforme.io>
2025-09-05 16:32:05 +0200

    +popover

--- views/admin/users.php
+++ views/admin/users.php
@@ -1,5 +1,6 @@
 <div class="user-page">
     <h1>Gestion des utilisateurs</h1>
+    <!-- TODO stats: total of borrows, average borrow per months?, average late return per months? -->
     <table class="user-table">
         <thead>
             <?php foreach ($fields as $field): ?>
@@ -14,35 +15,39 @@
                         <td><?php e($value) ?></td>
                     <?php endforeach; ?>
                     <td>
+                        <button popovertarget="borrow-list">(number of borrow) button Details</button>
+                        <!-- <div class="borrow-list-container"> -->
+                        <!-- get borrows -->
+                        <div id="borrow-list" popover>Contenu du popover
+                            <table class="user-table">
+                                <thead>
 
-                        <table class="user-table">
-                            <thead>
-
-                                <th>Title</th>
-                                <th>Borrow date</th>
-                                <th>Expected return</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Test</td>
-                                    <td>Test</td>
-                                    <td>Test</td>
-                                </tr>
-                                <tr>
-                                    <td>alibaba</td>
-                                    <td>21/10/2021</td>
-                                    <td>1 day</td>
-                                </tr>
-                                <tr>
-                                    <td>Marvel super hero</td>
-                                    <td>21/10/2021</td>
-                                    <td>retard de 12 jours</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                                    <th>Title</th>
+                                    <th>Borrow date</th>
+                                    <th>Expected return</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Test</td>
+                                        <td>Test</td>
+                                        <td>Test</td>
+                                    </tr>
+                                    <tr>
+                                        <td>alibaba</td>
+                                        <td>21/10/2021</td>
+                                        <td>1 day</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Marvel super hero</td>
+                                        <td>21/10/2021</td>
+                                        <td>retard de 12 jours</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- </div> -->
                     </td>
                     <td><button type="submit" class="btn btn-delete">Delete</button></td>
-
                 </tr>
             <?php endforeach; ?>
         </tbody>
