a1ba4af7e9ce1f107ecca297c7f19fcd4c7b6b56
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-09-08 16:12:23 +0200

    traduction

--- controllers/media_controller.php
+++ controllers/media_controller.php
@@ -12,7 +12,7 @@ function media_borrow()
 
     $user_id = null;
     if (!is_logged_in()) {
-        set_flash("error", "you must be logged in");
+        set_flash("error", "Veuillez vous connecter");
         redirect("auth/login");
     } else {
         $user_id = current_user_id();
@@ -24,7 +24,7 @@ function media_borrow()
     $media_id = intval(escape($_POST["id"]));
     $ret = get_media_stock_by_id($media_id);
     if (!$ret || $ret <= 0) {
-        set_flash("error", "item is out of stock");
+        set_flash("error", "Ce média n'est pas disponible");
         redirect("home");
     }
 
@@ -32,13 +32,13 @@ function media_borrow()
 
     //check media already borrowed by this user
     if (is_media_already_borrowed_by_user($media_id, $user_id)) {
-        set_flash("error", "You are already renting this media");
+        set_flash("error", "Vous avez déjà emprunté ce média");
         redirect("home");
     }
 
     //check user has rented less than 3
     if (get_borrow_count_by_user_id($user_id) >= 3) {
-        set_flash("error", "You are already renting the maximum number of medias");
+        set_flash("error", "Vous avez atteint le maximum d'emprunts par utilisateur (3)");
         redirect("home");
     }
 
@@ -47,18 +47,18 @@ function media_borrow()
 
     if (!borrow_media($media_id, $user_id)) {
         //failure
-        set_flash("error", "Something went wrong");
+        set_flash("error", "Quelque chose n'a pas fonctionné");
         error_logging(ErrorType::Error, "Failed to borrow media" . $media_id . " by user " . $user_id);
         redirect("home");
     }
 
 
     //TODO rework this part
-    $msg = "OK rent media: " . $media_id;
+    $msg = "Média emprunté avec succès " . $media_id;
     $data = [
         'title' => 'Profile',
         'message' => $msg,
-        'content' => 'Cette application est un starter kit PHP MVC développé avec une approche procédurale.'
+        'content' => "Merci d'avoir emprunté chez nous"
     ];
 
     load_view_with_layout('home/profile', $data);
@@ -88,21 +88,21 @@ function media_return()
 
     //check if media is already borrowed by user
     if (!is_media_already_borrowed_by_user($media_id, $user_id)) {
-        set_flash("error", "You didn't borrow this media : " . $media_id);
+        set_flash("error", "Vous ne pouvez pas rendre ce média : " . $media_id);
         redirect('home/profile');
     }
 
 
     if (!return_media($media_id, $user_id)) {
         //failure
-        set_flash("error", "Something went wrong");
+        set_flash("error", "Quelque chose s'est mal passé");
         error_logging(ErrorType::Error, "Failed to borrow media" . $media_id . " by user " . $user_id);
         redirect("home");
     }
 
 
     //TODO rework this part
-    $msg = "OK media returned: " . $media_id;
+    $msg = "Le média a bien été rendu: " . $media_id;
     $data = [
         'title' => 'Profile',
         'message' => $msg,
