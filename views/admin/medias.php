56d3272d285e24e1d68c5784facb68ae896617e6
Johan <johan.chevet@laplateforme.io>
2025-09-11 11:36:21 +0200

    added auto fill on search bar and moved to its own file

--- views/admin/medias.php
+++ views/admin/medias.php
@@ -9,31 +9,8 @@
         <div class="media-container"></div>
     </div>
 
-    <div class="search-container-wrapper">
-        <form class="search-container" method="GET">
-            <label for="title" hidden>Rechercher par titre</label>
-            <input class="search-input" type="text" id="title" name="title" placeholder="Rechercher">
-            <label for="type" hidden>Catégories</label>
-            <select name="type" id="type">
-                <option value="">Catégories</option>
-                <option value="Movie">Films</option>
-                <option value="Book">Livres</option>
-                <option value="Game">Jeux</option>
-            </select>
-            <label for="genre" hidden>Genres</label>
-            <select name="genre" id="genre">
-                <option value="">Genres</option>
-                <?php foreach (get_genre_values() as $genre): ?>
-                    <option value="<?= $genre ?>"><?= $genre ?></option>
-                <?php endforeach; ?>
-            </select>
-            <div class="checkbox-container">
-                <label for="available">Disponible</label>
-                <input class="checkbox" type="checkbox" id="available" name="available">
-            </div>
-            <button class="search-btn" type="submit"></button>
-        </form>
-    </div>
+    <?php include VIEW_PATH . '/search-bar/search-bar.php' ?>
+
     <div class="media-container">
 
         <table>
@@ -57,12 +34,13 @@
 
 
                         <td>
-                            <form action="<?=url('admin/delete_media') ?>" method="post">
-                            <input type="hidden" name="csrf_token" value="<?php echo csrf_token(); ?>">
-                            <input type="hidden" name="id" value="<?= $media['id'] ?>">
-                            <input type="hidden" name="redirect" value="admin/medias">
-                            <button type="submit" class="btn btn-alert">Supprimer</button>
-                        </form></td>
+                            <form action="<?= url('admin/delete_media') ?>" method="post">
+                                <input type="hidden" name="csrf_token" value="<?php echo csrf_token(); ?>">
+                                <input type="hidden" name="id" value="<?= $media['id'] ?>">
+                                <input type="hidden" name="redirect" value="admin/medias">
+                                <button type="submit" class="btn btn-alert">Supprimer</button>
+                            </form>
+                        </td>
                     </tr>
                 </tbody>
             <?php endforeach; ?>
